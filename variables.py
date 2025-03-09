11e9ae0c1f25026a4f9e2f6bf27e06eea4584848
jonny-big-money <chenjonathan62@gmail.com>
2025-03-09 10:24:04 -0400

    2nd commit

---
+++ variables.py
@@ -0,0 +1,14 @@
+x=6
+y=3
+sum=x+y
+product=x*y
+print(sum)
+print("The sum of ", x,"and ",y,"is ",sum)
+print("The product of ",x,"and",y,"is ",product)
+
+a=int(input("Enter your first number:"))
+b=int(input("Enter your second number:"))
+
+quotient=(a/b)
+
+print("The quotient of ", a,"and ",b,"is ",quotient)
